d528dfb96842c06c9b41e45cb7fa1b65b59bea42
joshiejack <joshjackwildman@gmail.com>
2021-07-28 12:12:17 +0100

    Update to 1.17.1

--- src/main/java/uk/joshiejack/daydreamer/Daydreamer.java
+++ src/main/java/uk/joshiejack/daydreamer/Daydreamer.java
@@ -3,17 +3,16 @@ package uk.joshiejack.daydreamer;
 import net.minecraftforge.event.entity.player.SleepingTimeCheckEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 @Mod("daydreamer")
 public class Daydreamer {
     public Daydreamer() {
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, ()-> Pair.of(()-> FMLNetworkConstants.IGNORESERVERONLY, (net, save)->true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (n, s) -> true));
     }
 
     @SubscribeEvent
